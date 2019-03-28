0f02117df90cc6336e9a258900a3a0d54453f2e9
huangjh <2372635607@qq.com>
2019-03-28 15:17:40 +0800

    [日志工具，配置] log4j2库配置；

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
 import com.example.demo.bean.UserInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +15,9 @@ import java.util.Map;
 @RestController
 @RequestMapping("/index/")
 public class IndexController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(IndexController.class);
+
     @Value(value = "${zy.secret}")
     private String value;
 
@@ -22,19 +27,20 @@ public class IndexController {
     @RequestMapping("test")
     public String index() {
         System.out.println("hello index...");
+        LOG.info("这是 IndexController.test");
         return "hello spring boot";
     }
 
     @RequestMapping("find/{id}/{name}")
-    public UserInfo find(@PathVariable Integer id, @PathVariable String  name){
+    public UserInfo find(@PathVariable Integer id, @PathVariable String name) {
         UserInfo userInfo = new UserInfo();
         userInfo.setId(id);
         userInfo.setName(name);
-        return  userInfo;
+        return userInfo;
     }
 
     @RequestMapping("get")
-    public Map<String, Object> get(@RequestParam String name){
+    public Map<String, Object> get(@RequestParam String name) {
         Map<String, Object> map = new HashMap<>();
         map.put("name", name);
         map.put("value", value);
